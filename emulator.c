c7212c10a2de6e3f81642ccb49dcfba4b7e22e18
Jack Huang <jintaohuang72@gmail.com>
2022-12-08 22:29:49 -0500

    Added some basic functions

--- emulator.c
+++ emulator.c
@@ -1,2 +1,44 @@
 #include "emulator.h"
 
+/*
+Condition Code based functions
+*/
+int parity(int x)
+{
+	int i;
+    int size = 8;
+	int p = 0;
+	x = (x & ((1<<size)-1));
+	for (i=0; i<size; i++)
+	{
+		if (x & 0x1) p++;
+		x = x >> 1;
+	}
+	return (0 == (p & 0x1));
+}
+void set8080ConditionCodes(uint16_t answer, State8080 *state){
+    state->cc.z = ((answer & 0xff) == 0);
+    state->cc.s = ((answer & 0x80) != 0);
+    state->cc.cy = (answer > 0xff);
+    state->cc.p = parity(answer&0xff);
+}
+/*
+Initialize 8080 emulator.
+*/
+State8080* init8080(void)
+{
+    State8080* state = calloc(1,sizeof(State8080));
+	state->memory = malloc(0x10000);  //16K
+	return state;
+}
+int emulate8080Op(State8080* state){
+    return 0;
+}
+
+
+
+
+int main()
+{
+    return 0;
+}
--- emulator.h
+++ emulator.h
@@ -9,16 +9,16 @@ headers.
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-
+#include <stdbool.h>
 /*
 8080 Condition codes. Set after a completed instruction. Used by conditional branching instructions.
 */
 typedef struct ConditionCodes {
-    uint8_t z: 1;       // zero. z = 1 if res = 0
-    uint8_t s: 1;       // sign. s = 1 if bit7 = 1
-    uint8_t p: 1;       // parity. p = 1 if even
-    uint8_t cy: 1;      // carry. cy =1 if need a carry
-    uint8_t ac: 1;      // auxillary carry. used mostly for binary coded decimal maths
+    bool z: 1;       // zero. z = 1 if res = 0
+    bool s: 1;       // sign. s = 1 if bit7 = 1
+    bool p: 1;       // parity. p = 1 if num of '1' bit is even
+    bool cy: 1;      // carry. cy =1 if need a carry
+    bool ac: 1;      // auxillary carry. used mostly for binary coded decimal maths
     uint8_t pad: 3;     
 } ConditionCodes;
 
@@ -38,11 +38,24 @@ typedef struct State8080
     uint8_t int_enable;
 } State8080;
 
+State8080* init8080(void);
+/*
+Parity function. Returns true if number of 1 bit is even
+*/
+int parity(int x, int size);
 /*
 Emulates the 8080 cpu. Finds the current instruction in memory,
-executes the opcode, and increment pc by 1
+executes the opcode, and increment pc by 1. Return 0;
 */
 int emulate8080Op(State8080* state);
 
+/*
+Sets the condition code of the state based on the answer at the end of the operation
+*/
+void set8080ConditionCodes(uint16_t answer, State8080 *state);
 
+/*
+Read file into memory, given an offset
+*/
+void readFileIntoMemoryAt(State8080 *state, char* filename, uint32_t offset);
 #endif
